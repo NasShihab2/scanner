c7dcad9ec392856a12bfd5dddcdb2be97e169cb9
Nas <nas.shihab92@gmail.com>
2023-08-27 01:00:40 -0600

    update

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:scanner/homepage_controller.dart';
-import 'package:scanner/second_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
